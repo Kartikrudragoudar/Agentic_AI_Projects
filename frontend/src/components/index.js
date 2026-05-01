78b63cc53c7f3d22b8f0f4c45e3637eb9045f1e9
Kartik Rudragoudar <91456301+Kartikrudragoudar@users.noreply.github.com>
2026-05-01 20:41:57 +0530

    Delete frontend directory

--- frontend/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as SpatialCard } from './SpatialCard';
-export { default as ActionButton } from './ActionButton';
-export { default as MarketTicker } from './MarketTicker';
-export { default as GlassInput } from './GlassInput';
-export { default as Chip } from './Chip';
-export { default as PerspectiveChart } from './PerspectiveChart';
